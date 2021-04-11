a9f00438aa8e589c8a1e9a70bcbac79dee34fa48
vue-mint-move(new) <41984768+zhaorongkui@users.noreply.github.com>
2021-04-11 09:39:55 +0800

    test测试一下

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <h1>'test一下888888888888888888‘</h1>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
